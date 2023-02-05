b6604ef3fbc8f3041f66a98de97a5a89241aaa89
KhanhGN <kgnguyen@myseneca.ca>
2023-02-05 14:44:34 +0700

    feat: i have no idea how this is working, but this is magik

--- src/components/Test.component.tsx
+++ src/components/Test.component.tsx
@@ -7,42 +7,48 @@ const Static = () => {
 	return <div>Static so only render once</div>;
 };
 
+const Home = () => {
+	console.log('Home');
+	return (
+		<div>
+			<h1>Home</h1>
+			<Link to='/dashboard/about'>About</Link>
+			<Link to='/dashboard/contact'>Contact</Link>
+		</div>
+	);
+};
+
+const About = () => {
+	console.log('About');
+	return (
+		<div>
+			<h1>About</h1>
+			<Link to='/dashboard'>Home</Link>
+			<Link to='/dashboard/contact'>Contact</Link>
+		</div>
+	);
+};
+
+const Contact = () => {
+	console.log('Contact');
+	return (
+		<div>
+			<h1>Contact</h1>
+			<Link to='/dashboard'>Home</Link>
+			<Link to='/dashboard/about'>About</Link>
+		</div>
+	);
+};
+
 const Test = () => {
 	return (
 		<div>
 			<Static />
 			<BrowserRouter>
 				<Routes>
-					<Route
-						path='/dashboard'
-						element={
-							<div>
-								<h1>Home</h1>
-								<Link to='/dashboard/about'>About</Link>
-								<Link to='/dashboard/contact'>Contact</Link>
-							</div>
-						}
-					/>
-					<Route
-						path='/dashboard/about'
-						element={
-							<div>
-								<h1>About</h1>
-								<Link to='/dashboard'>Home</Link>
-								<Link to='/dashboard/contact'>Contact</Link>
-							</div>
-						}
-					/>
-					<Route
-						path='/dashboard/contact'
-						element={
-							<div>
-								<h1>Contact</h1>
-								<Link to='/dashboard'>Home</Link>
-								<Link to='/dashboard/about'>About</Link>
-							</div>
-						}
-					/>
+					<Route path='/dashboard' element={<Home />} />
+					<Route path='/dashboard/about' element={<About />} />
+					<Route path='/dashboard/contact' element={<Contact />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
